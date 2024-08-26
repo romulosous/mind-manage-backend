cf8a64b8a78e53388e0524f8be805b98b1ab2d50
Pedrohrd <pedrohrd87@gmail.com>
2024-08-26 09:34:54 -0300

    feat: add configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common'
 
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { PsychologistModule } from './psychologist/psychologist.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PsychologistModule],
 })
 export class AppModule {}
---
+++ src/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    })
+  }
+  async onModuleInit() {
+    return this.$connect()
+  }
+
+  async onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
