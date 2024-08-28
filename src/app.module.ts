02118b359ad05587b4d86961a0ff2491fcf9acec
Pedrohrd <pedrohrd87@gmail.com>
2024-08-28 13:21:36 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common'
-import { APP_GUARD } from '@nestjs/core'
 
 import { AuthModule } from './auth/auth.module'
-import { JwtAuthGuard } from './auth/jwt-auth.guard'
 import { PatientModule } from './patient/patient.module'
 import { PsychologistModule } from './psychologist/psychologist.module'
 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors({
+    credentials: true,
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +24,7 @@ async function bootstrap() {
       },
     }),
   )
+  app.use(cookieParser())
   await app.listen(process.env.PORT || 3333)
 }
 bootstrap()
